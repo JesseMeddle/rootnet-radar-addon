30b49742368893b106423604be4653e4d24af75d
Uriah <73462184+UriahC@users.noreply.github.com>
2021-09-08 12:57:43 +0200

    Messed up something, here's the fix

--- src/main/java/c/uriah/radar/RadarAddon.java
+++ src/main/java/c/uriah/radar/RadarAddon.java
@@ -2,9 +2,13 @@ package c.uriah.radar;
 
 import dev.rootnet.addons.api.addon.Addon;
 import dev.rootnet.addons.api.annotations.RootnetAddon;
+import dev.rootnet.addons.api.annotations.RootnetSetting;
 
 import org.apache.logging.log4j.Level;
 
+import java.io.IOException;
+
+@SuppressWarnings("unused")
 @RootnetAddon(name = "Radar", author = "Uriah C.", version = "#VERSION#")
 
 public final class RadarAddon extends Addon {
--- src/main/java/c/uriah/radar/RadarModule.java
+++ src/main/java/c/uriah/radar/RadarModule.java
@@ -15,30 +15,32 @@ public final class RadarModule extends AddonModule {
 	
 	public int y;
 	private static Minecraft mc = Minecraft.getMinecraft();
-	FontRenderer fr = mc.fontRenderer;
-    @SuppressWarnings("rawtypes")
-	Iterator iterator = mc.world.playerEntities.iterator();
+	FontRenderer fr = mc.fontRenderer;	
 	
-    @Override
-    public void renderOverlay() {	
-			int y=2;
-			Gui.drawRect(108, 12, 2,  29, 0x50000000);
-			Gui.drawRect(106, 14, 4,  27, new Color(50,205,50, 240).getRGB());
-			fr.drawString("Radar-Addon", 6, 17, 0xffffff);
+	@SuppressWarnings({ "rawtypes" })
+	@Override
+    public void renderOverlay() {
+		
+		int y=2;
+		Iterator iterator = mc.world.playerEntities.iterator();
+		Gui.drawRect(108, 12, 2,  29, 0x50000000);
+		Gui.drawRect(106, 14, 4,  27, new Color(50,205,50, 240).getRGB());
+		fr.drawString("Radar-Addon", 6, 17, 0xffffff);
 
-			for(EntityPlayer nextPlayer : mc.world.playerEntities){
-		         do{
-		            if (!iterator.hasNext()) {
-		            	Gui.drawRect(108, y+27, 2, y+29, 0x50000000);
-						return;
-		            }
+		for (EntityPlayer nextPlayer : mc.world.playerEntities) {		
+		    do{
+		        if (!iterator.hasNext()) {
+		            Gui.drawRect(108, y+27, 2, y+29, 0x50000000);
+					return;
+		        }
 		            nextPlayer = (EntityPlayer)iterator.next();
-		         }
-		         while(nextPlayer.getName().equals(mc.player.getName()));
-		            Gui.drawRect(108, y+27, 2, y+38, 0x50000000);
-					Gui.drawRect(106, y+27, 4, y+38, 0x50000000);
-		            fr.drawStringWithShadow(nextPlayer.getName()+" " +(int)mc.player.getDistance(nextPlayer), 5, y+29, 0xffffff);
-		            y+= fr.FONT_HEIGHT+2;	            
-		      }			
+		    } 
+		    
+		    while(nextPlayer.getName().equals(mc.player.getName()));
+		    Gui.drawRect(108, y+27, 2, y+38, 0x50000000);
+			Gui.drawRect(106, y+27, 4, y+38, 0x50000000);
+		    fr.drawStringWithShadow(nextPlayer.getName()+" " +(int)mc.player.getDistance(nextPlayer), 5, y+29, 0xffffff);
+		    y+= fr.FONT_HEIGHT+2;	
 		}
+	}
 }
